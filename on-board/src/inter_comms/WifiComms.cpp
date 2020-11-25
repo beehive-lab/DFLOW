13f56f5c25e62f6a33024b838c697824ec124f12
orl <olandes@hotmail.co.uk>
2020-11-25 15:35:36 +0000

    WIP: Fix polling and sending

--- on-board/src/inter_comms/WifiComms.cpp
+++ on-board/src/inter_comms/WifiComms.cpp
@@ -1,4 +1,5 @@
 #include "WifiComms.h"
+#include <cstring>
 
 using namespace std;
 
@@ -6,17 +7,17 @@ int other_socket;
 
 int this_socket;
 
-int send(char data[1024]) {
-    int status = send(other_socket, data, 1024, 0);
+int send(char *data) {
+    int status = send(other_socket, data, strlen(data), 0);
 
     if (status == -1) {
         int connect_status = listen_to_connection(8080);
         if (connect_status == -1) {
-            cerr<<"Error connecting to the socket";
+            cerr << "Error connecting to the socket" << endl;
             exit(1);
         }
 
-        send(other_socket, data, 1024, 0);
+        send(other_socket, data, strlen(data), 0);
     }
 
     return 0;
@@ -26,17 +27,19 @@ int polling() {
 
     struct pollfd poll_fd[1];
 
-    poll_fd[0].fd = this_socket;
+    poll_fd[0].fd = other_socket;
     poll_fd[0].events = POLLIN;
 
-    poll(poll_fd, 1, -1);
+//    poll(poll_fd, 1, -1);
 
     char buffer[1024];
-    if (poll_fd[0].revents & POLLIN) {
-        recv(other_socket, buffer, 1024, 0);
-    }
+//    if (poll_fd[0].revents & POLLIN) {
+    recv(other_socket, buffer, 1024, 0);
+//    }
 
-    cout<<buffer;
+    cout << buffer << endl;
+
+    send(buffer);
 
     // do something with the data. send it further.
 
@@ -55,13 +58,13 @@ int listen_to_connection(int port) {
     char const *port_char = tmp.c_str();
 
     if (getaddrinfo(nullptr, port_char, &hints, &server_info) != 0) {
-        cerr<<"Error retrieving information from server";
+        cerr << "Error retrieving information from server" << endl;
         exit(1);
     }
 
     int socket_fd = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
     if (socket_fd == -1) {
-        cerr<<"Error creating the socket";
+        cerr << "Error creating the socket" << endl;
         exit(1);
     }
 
@@ -69,7 +72,7 @@ int listen_to_connection(int port) {
 
     int bind_status = bind(socket_fd, server_info->ai_addr, server_info->ai_addrlen);
     if (bind_status == -1) {
-        cerr<<"Error binding the address to the socket";
+        cerr << "Error binding the address to the socket" << endl;
         exit(1);
     }
 
@@ -81,16 +84,16 @@ int listen_to_connection(int port) {
 
     int listen_status = listen(socket_fd, 1);
     if (listen_status == -1) {
-        cerr<<"Error listening to the port";
+        cerr << "Error listening to the port" << endl;
         exit(1);
     }
 
     struct sockaddr_storage second_address{};
     int address_len = sizeof(second_address);
-    int new_socket_fd = accept(socket_fd, (struct sockaddr *)&second_address, (socklen_t*) &address_len);
+    int new_socket_fd = accept(socket_fd, (struct sockaddr *) &second_address, (socklen_t *) &address_len);
 
     if (new_socket_fd == -1) {
-        cerr<<"Error accepting connection";
+        cerr << "Error accepting connection" << endl;
         exit(1);
     }
 
