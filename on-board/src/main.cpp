189b59c84288dc6e248174b104d1e68bcb7eb7e7
Radu Andra <radu-tudor.andra@student.manchester.ac.uk>
2021-02-02 18:35:48 +0000

    Set up pipe between threads to send CAN data to client

--- on-board/src/main.cpp
+++ on-board/src/main.cpp
@@ -1,9 +1,10 @@
 #include "dataretriever.hpp"
 #include <unistd.h>
+#include <string>
 #include <thread>
 #include <canmodule.hpp>
 #include <WifiComms.h>
-#include <BluetoothComms.g>
+#include <BluetoothComms.h>
 
 using namespace std;
 
@@ -14,16 +15,22 @@ void retrieve(int *pip)
     retriever.listen(pip);
 }
 
-void listen(int *pip)
+void inter_coms_send(int *pip)
 {
     CAN_example_message received_message = CAN_example_message();
     char ostring[200];
+    WifiComms wifi_socket = WifiComms(true);
+    wifi_socket.establish_connection(8080);
+    char wifi_buffer[1024];
     while(true)
     {
         read(pip[0],ostring,200);
         received_message.decodeFromPipe(ostring);
-
-        std::cout<<"From main: "<<received_message.temperature<<std::endl;
+        std::string message_string = std::string();
+        message_string = "Received message with data: temperature = " + std::to_string(received_message.temperature) + "; average radius = " + std::to_string(received_message.average_radius) + "; switch value = " + std::to_string(received_message.switch_value);
+        wifi_socket.receive(wifi_buffer);
+        if(strcmp(wifi_buffer,"Listening") == 0)
+            wifi_socket.send((char*)message_string.c_str());
     }
 }
 
@@ -33,7 +40,7 @@ int main() {
     pipe(pip);
 
     std::thread first(retrieve,pip);
-    std::thread second(listen,pip);
+    std::thread second(inter_coms_send,pip);
 
     first.join();
     second.join();
