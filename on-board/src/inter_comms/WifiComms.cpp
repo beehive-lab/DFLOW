a7da7278ee5fde6547888c9fb8935a5f6a9e4593
Maxi17 <maxi.gama@yahoo.com>
2021-04-05 03:21:15 +0300

    Another try at making travis succeed 3

--- on-board/src/inter_comms/WifiComms.cpp
+++ on-board/src/inter_comms/WifiComms.cpp
@@ -252,7 +252,7 @@ int WifiComms::establish_connection(int port) {
     SSL_library_init();
     OpenSSL_add_all_algorithms();
     SSL_load_error_strings();
-    const SSL_METHOD *method = TLS_server_method();
+    const SSL_METHOD *method = SSLv23_server_method();
     context = SSL_CTX_new(method);
 
     if (!context) {
