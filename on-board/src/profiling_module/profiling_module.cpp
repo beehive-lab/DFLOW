e4a71de501ef77e75858643b67114c94f4bf7c23
Radu Andra <radu.andra1999@gmail.com>
2021-04-09 12:26:48 +0100

    Add initial code

---
+++ on-board/src/profiling_module/profiling_module.cpp
@@ -0,0 +1,79 @@
+//#include "stdlib.h"
+#include <iostream>
+#include <ifstream>
+#include <string>
+#include "sys/times.h"
+#include "sys/vtimes.h"
+
+static clock_t lastCPU, lastSysCPU, lastUserCPU;
+static int numProcessors;
+
+void init()
+{
+    ofstream data_file;
+    struct tms timeSample;
+    char line[128];
+
+    lastCPU = times(&timeSample);
+    lastSysCPU = timeSample.tms_stime;
+    lastUserCPU = timeSample.tms_utime;
+
+    data_file.open("/proc/cpuinfo");
+    numProcessors = 0;
+    while(getline(line,data_file) != NULL){
+        if (strncmp(line, "processor", 9) == 0) numProcessors++;
+    }
+
+    data_file.close();
+}
+
+double getCurrentValue(){
+    struct tms timeSample;
+    clock_t now;
+    double percent;
+
+    now = times(&timeSample);
+    if (now <= lastCPU || timeSample.tms_stime < lastSysCPU ||
+        timeSample.tms_utime < lastUserCPU){
+        //Overflow detection. Just skip this value.
+        percent = -1.0;
+    }
+    else{
+        percent = (timeSample.tms_stime - lastSysCPU) +
+            (timeSample.tms_utime - lastUserCPU);
+        percent /= (now - lastCPU);
+        percent /= numProcessors;
+        percent *= 100;
+    }
+    lastCPU = now;
+    lastSysCPU = timeSample.tms_stime;
+    lastUserCPU = timeSample.tms_utime;
+
+    return percent;
+}
+
+
+int parseLine(char* line){
+    // This assumes that a digit will be found and the line ends in " Kb".
+    int i = strlen(line);
+    const char* p = line;
+    while (*p <'0' || *p > '9') p++;
+    line[i-3] = '\0';
+    i = atoi(p);
+    return i;
+}
+
+int getValue(){ //Note: this value is in KB!
+    FILE* file = fopen("/proc/self/status", "r");
+    int result = -1;
+    char line[128];
+
+    while (fgets(line, 128, file) != NULL){
+        if (strncmp(line, "VmSize:", 7) == 0){
+            result = parseLine(line);
+            break;
+        }
+    }
+    fclose(file);
+    return result;
+}
